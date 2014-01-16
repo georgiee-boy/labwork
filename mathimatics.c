1b36d6cbd745d8aac2aaf4fe5118cd72f919519d
Yorgo David Samaras <P12216289@GH583-012.lec-admin.dmu.ac.uk>
2014-01-16 09:59:04 +0000

    Fixed all errors in the mathimatics code

--- mathimatics.c
+++ mathimatics.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	int a, b, addition, subtraction, multiplication, devision;
+	int a, b, addition, subtraction, multiplication, division;
 	
 	printf("Enter 2 integers\n");
 	scanf(" %i %i", &a, &b);
@@ -11,42 +11,42 @@ int main()
 	printf("Addition:  \n\n");
 
 	addition = a + b;
-	printf("%i + %i = %i", a, b, addition);
+	printf("%i + %i = %i\n", a, b, addition);
 	
 	printf("Subtraction:  \n\n");
 	
 	if(a < b){
 		subtraction = b - a;
 		
-		printf(" %i - %i = %i", b, a, subtraction);
+		printf(" %i - %i = %i\n", b, a, subtraction);
 	}
 
 	else{
 		subtraction = a - b; 
 	
-		printf(" %i - %i = %i", a, b, subtraction);
+		printf(" %i - %i = %i\n", a, b, subtraction);
 		
 		printf("Multiplication:  \n\n");
 	}
 		
 	if(a < b){
 		multiplication = b * a;
-		printf(" %i X %i = %i", b, a, multiplication);
+		printf(" %i X %i = %i\n", b, a, multiplication);
 	}
 	else{
 		multiplication = a * b;
-		printf(" %i X %i = %i", a, b, multiplication);
+		printf(" %i X %i = %i\n", a, b, multiplication);
 	}
 		
 	printf("Division:  \n\n");
 	
 	if(a < b){
 		division = b / a;
-		printf(" %i / %i = %i", b, a, division);
+		printf(" %i / %i = %i\n", b, a, division);
 	}
 	else{
 		division = a / b;
-		printf(" %i / %i = %i", a, b, division);
+		printf(" %i / %i = %i\n", a, b, division);
 	}
 }
 	
