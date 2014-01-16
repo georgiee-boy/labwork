b63b744e748a226269fe19ebdee9089b4e299bff
Yorgo David Samaras <P12216289@GH583-012.lec-admin.dmu.ac.uk>
2014-01-16 09:53:30 +0000

    Added the multiplication method to the program

--- mathimatics.c
+++ mathimatics.c
@@ -2,23 +2,42 @@
 
 int main()
 {
-	int a, b, addition, subtraction, multiply, devide;
+	int a, b, addition, subtraction, multiplication, devide;
 	
 	printf("Enter 2 integers\n");
 	scanf(" %i %i", &a, &b);
 	
+	
+	printf("Addition:  \n\n");
+
 	addition = a + b;
+	printf("%i + %i = %i", a, b, addition);
 	
-	printf("%i + %i = %i", a, a, addition);
+	printf("Subtraction:  \n\n");
 	
-	if(a < b)
+	if(a < b){
 		subtraction = b - a;
 		
 		printf(" %i - %i = %i", b, a, subtraction);
+	}
 
-	else
-		subtraction = a - b;
+	else{
+		subtraction = a - b; 
 	
 		printf(" %i - %i = %i", a, b, subtraction);
+		
+		printf("Multiplication:  \n\n");
+	}
+		
+	if(a < b){
+		multiplication = b * a;
+		printf(" %i X %i = %i", b, a, multiplication);
+	}
+	else{
+		multiplication = a * b;
+		printf(" %i X %i = %i", a, b, multiplication);
+	}
+		
+		
 }
 	
